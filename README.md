32200b993657e8cbd3289877cd05cd384486bd2f
CC02MACMINI002 <cc02macmini002@CC02MACMINI002.local>
2020-09-23 11:32:28 +0700

    Add code to preprocess image

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# preprocess_img
+# Preprocess_img
+
+conda create --name [env name] python==3.7
+
+pip install -r requirement.txt
+
+run code with: 
+
+python demo.py --input [path to input image] --output [path to save image] --mode [mode use for enchanging image #mode = IAGCWD or mode = LLIE]
--- demo.py
+++ demo.py
@@ -1,4 +1,90 @@
+import argparse
 import os
 import cv2
+import numpy as np
 
-print('test on demo')
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Demo a face recognition')
+
+    parser.add_argument('--input',
+                        help='the dir to img need to be proccessed')
+    parser.add_argument('--output',
+                        help='the dir to img output')
+    parser.add_argument('--mode',
+                        help='mode IAGCWD or LLIE',
+                        default='IAGCWD', type=str)
+    parser.add_argument("-l", '--lambda_', default=0.15, type=float,
+                    help="the weight for balancing the two terms in the illumination refinement optimization objective.")
+    parser.add_argument("-ul", "--lime", action='store_true',
+                        help="Use the LIME method. By default, the DUAL method is used.",
+                        default=False)
+    parser.add_argument("-g", '--gamma', default=0.6, type=float,
+                        help="the gamma correction parameter.")
+    parser.add_argument('--sigma', default=3, type=int,
+                        help="Spatial standard deviation for spatial affinity based Gaussian weights.")
+    parser.add_argument("-bc", default=1, type=float,
+                        help="parameter for controlling the influence of Mertens's contrast measure.")
+    parser.add_argument("-bs", default=1, type=float,
+                        help="parameter for controlling the influence of Mertens's saturation measure.")
+    parser.add_argument("-be", default=1, type=float,
+                        help="parameter for controlling the influence of Mertens's well exposedness measure.")
+    parser.add_argument("-eps", default=1e-3, type=float,
+                        help="constant to avoid computation instability.")
+# LLIE only use with low-light Image
+    args = parser.parse_args()
+    return args
+
+
+def main():
+
+    img = cv2.imread(args.input, 1)
+    YCrCb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    Y = YCrCb[:,:,0]
+    # Determine whether image is bright or dimmed
+    threshold = 0.4
+    exp_in = 112 # Expected global average intensity 
+    M,N = img.shape[:2]
+    mean_in = np.sum(Y/(M*N)) 
+    t = (mean_in - exp_in)/ exp_in
+    
+    # Process image for gamma correction
+    img_output = None
+    if args.mode == 'IAGCWD':
+        from IAGCWD.process import process_bright
+        from IAGCWD.process import process_dimmed
+
+        if t < -threshold:
+            # Dimmed Image
+            print ('Image is Dimmed {}'.format(t))
+            result = process_dimmed(Y)
+            YCrCb[:,:,0] = result
+            img_output = cv2.cvtColor(YCrCb,cv2.COLOR_YCrCb2BGR)
+        elif t > threshold:
+            # Bright Image
+            print ('Image is Bright {}'.format(t))
+            result = process_bright(Y)
+            YCrCb[:,:,0] = result
+            img_output = cv2.cvtColor(YCrCb,cv2.COLOR_YCrCb2BGR)
+        else:
+            print('Image is normal {}'.format(t))
+            img_output = img
+
+    elif args.mode == 'LLIE':
+        from LLIE.process import enhance_image_exposure
+        if t < -threshold:
+            enhanced_image = enhance_image_exposure(
+                img, args.gamma, args.lambda_, not args.lime,
+                sigma=args.sigma, bc=args.bc, bs=args.bs, be=args.be, eps=args.eps
+            )
+            img_output = enhanced_image
+        else:
+            img_output = img
+
+    cv2.imwrite(args.output, img_output)
+    return 0
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main()
